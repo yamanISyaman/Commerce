e3864ef7662dda31595d4892983d5c5d65d96c68
yamanomobile <yamano@mobile.com>
2023-06-04 13:01:08 +0000

    created auction model

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class auction(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField()
+
