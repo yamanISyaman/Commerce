70bd32d08b89f35f39947bc29e39249b308df136
yamanomobile <yamano@mobile.com>
2023-06-26 15:15:14 +0000

    added modles to admin page

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Auction, Bid, Comment, WatchList
+
+
+admin.site.register(User)
+admin.site.register(Auction)
+admin.site.register(Comment)
+admin.site.register(Bid)
+admin.site.register(WatchList)
--- auctions/models.py
+++ auctions/models.py
@@ -15,7 +15,10 @@ class Auction(models.Model):
     category = models.CharField(max_length=30)
     closed = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
-    winner = models.ForeignKey(User, null=True, default=None, on_delete=models.SET_NULL, related_name="winner")
+    winner = models.ForeignKey(User, blank=True, null=True, default=None, on_delete=models.SET_NULL, related_name="winner")
+
+    def __str__(self):
+        return self.title
 
 
 class Bid(models.Model):
@@ -24,6 +27,9 @@ class Bid(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.user} with {self.price} on {self.auction}"
+
 
 class Comment(models.Model):
     text = models.CharField(max_length=400)
@@ -31,7 +37,13 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f"{self.user} on {self.auction}"
+
 
 class WatchList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} added {self.auction}"
